2c5f6bd4053768f4b28196095f8730cbfb1843e8
Abdelrahman Hussein <husseina@greenitglobe.com>
2017-11-05 03:54:17 +0100

    fix longjob test

--- tests/test_services/test_longjobs_actions/actions.py
+++ tests/test_services/test_longjobs_actions/actions.py
@@ -1,5 +1,5 @@
 def init_actions_(service, args):
-
+    
     """
 
     this needs to returns an array of actions representing the depencies between actions.
@@ -50,7 +50,29 @@ def test(job):
         j.sal.fs.copyFile("{}.bak".format(source_config), source_config)
         # call actor update
         original_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long1', state='running'))
+        cl.updateActor(data={}, actor='longjobsact', repository=repo)cl = j.clients.atyourservice.get().api.ays
+        repo = 'sample_repo_longjobs'
+        repos = cl.listRepositories().json()
+
+        path = ''
+        for repo_info in repos:
+            if repo_info['name'] == repo:
+                path = repo_info['path']
+                break
+        # start without any long jobs configured
+        config_path = j.sal.fs.joinPaths(path, 'actorTemplates', 'longjobsact')
+        source_config = j.sal.fs.joinPaths(config_path, 'config.yaml')
+        modified_config = j.sal.fs.joinPaths(config_path, 'config.modified.yaml')
+        j.sal.fs.copyFile(source_config, "{}.bak".format(source_config))
+        j.sal.fs.writeFile(source_config, "")
+        cl.executeBlueprint(data=None, repository=repo, blueprint='test_longjobsact.yaml')
+        # update config to have a long running job
+        j.sal.fs.copyFile("{}.bak".format(source_config), source_config)
+        # call actor update
+        original_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long1', state='running'))
         cl.updateActor(data={}, actor='longjobsact', repository=repo)
+        time.sleep(2)
+
         updated_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long1', state='running'))
         if updated_nr_of_jobs - original_nr_of_jobs != 1:
             failures.append("Updating actor did not add long jobs")
@@ -67,6 +89,7 @@ def test(job):
         original_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long2', state='running'))
         # call actor update
         cl.updateActor(data={}, actor='longjobsact', repository=repo)
+        time.sleep(2)
 
         # check number of jobs
         updated_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long2', state='running'))
@@ -78,6 +101,42 @@ def test(job):
 
         # call actor update
         cl.updateActor(data={}, actor='longjobsact', repository=repo)
+        time.sleep(2)
+
+        updated_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long2', state='running'))
+        if updated_nr_of_jobs != original_nr_of_jobs:
+            failures.append('Updating actor does not remove long jobs')
+
+        updated_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long1', state='running'))
+        if updated_nr_of_jobs - original_nr_of_jobs != 1:
+            failures.append("Updating actor did not add long jobs")
+
+        # test update of the actor, after adding new long job to the config
+        # update the actor's config
+        config_path = j.sal.fs.joinPaths(path, 'actorTemplates', 'longjobsact')
+        source_config = j.sal.fs.joinPaths(config_path, 'config.yaml')
+        j.sal.fs.copyFile(source_config, '{}.bak'.format(source_config))
+        j.sal.fs.copyFile(modified_config, source_config)
+
+        # get the number of jobs for the new long job actor
+        # this assumes that the test runs on the same machine where the ays is running
+        original_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long2', state='running'))
+        # call actor update
+        cl.updateActor(data={}, actor='longjobsact', repository=repo)
+        time.sleep(2)
+
+        # check number of jobs
+        updated_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long2', state='running'))
+        if updated_nr_of_jobs - original_nr_of_jobs != 1:
+            failures.append('Updating actor does not add long jobs')
+
+        # now lets revert to the original config to remove the newly configured long running job
+        job.service.executor.execute('cp {}.bak {}'.format(source_config, source_config))
+
+        # call actor update
+        cl.updateActor(data={}, actor='longjobsact', repository=repo)
+        time.sleep(2)
+        
         updated_nr_of_jobs = len(j.core.jobcontroller.db.jobs.list(actor='longjobsact', action='long2', state='running'))
         if updated_nr_of_jobs != original_nr_of_jobs:
             failures.append('Updating actor does not remove long jobs')
