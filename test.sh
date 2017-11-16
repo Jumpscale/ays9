44aa4c57e34555f56b2f2546ee60542a4e2ce77c
Abdelrahman Hussein <husseina@greenitglobe.com>
2017-11-16 15:14:40 +0100

    fix daily build

--- test.sh
+++ test.sh
@@ -25,12 +25,8 @@ done
 # running testsuite
 
 echo "Running ays core tests"
-js9 "from ays_testrunner.testrunner import AYSTestRunnerFactory;AYSTestRunnerFactory.get(name='core').run()"
+js9 "from ays_testrunner.testrunner import AYSTestRunnerFactory;AYSTestRunnerFactory.get(name='core', execution_type='threaded').run()"
 
-if [ -n $RUNTYPE ] && [ $RUNTYPE == "cron" ]; then
-  echo "Running ays non-core tests"
-  js9 "from ays_testrunner.testrunner import AYSTestRunnerFactory;import json;AYSTestRunnerFactory.get(name='none-core', execution_type='threaded', config={'BACKEND_ENV_CLEANUP': True, 'BACKEND_ENV': dict([(key.replace('BACKEND_', ''), value) for key, value in json.load(open('/hostcfg/ays_testrunner.json'))['BACKEND_ENV'].items()])}).run()"
-fi
 
 
 
