30fd50e6655df1f53b50a5783499ec061a23006f
Hossameldeen <hossamabdalh@gmail.com>
2017-11-28 11:08:03 +0000

    update the usage of get SSHBased function to follow new structure

--- templates/os/os.ssh.ubuntu/actions.py
+++ templates/os/os.ssh.ubuntu/actions.py
@@ -29,9 +29,7 @@ def install(job):
 
     # used the login/password information from the node to first connect to the node and then authorize the sshkey for root
     executor = j.tools.executor.getSSHBased(addr=node.model.data.ipPublic, port=service.model.data.sshPort,
-                                            login=node.model.data.sshLogin, passwd=password,
-                                            allow_agent=True, look_for_keys=True, timeout=5, usecache=False,
-                                            passphrase=passphrase, key_filename=key_path)
+                                            timeout=5, usecache=False,)
     executor.prefab.system.ssh.authorize("root", sshkey.model.data.keyPub)
     # Reset prefab instance to use root for upcoming prefab executions instead of normal user
     j.tools.prefab.resetAll()
