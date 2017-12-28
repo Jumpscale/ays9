0f3d63066c31968b589b6565078e20c73a54deb6
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-12-28 10:33:03 +0100

    fix sshkey template

--- templates/clients/sshkey/actions.py
+++ templates/clients/sshkey/actions.py
@@ -13,7 +13,7 @@ def input(job):
             raise j.exceptions.Input(message="Cannot find ssh key:%s for service:%s" %
                                      (path, job.service), level=1, source="", tags="", msgpub="")
 
-        args['key.path'] = j.sal.fs.joinPaths(job.service.path, "id_rsa")
+        args['key.path'] = j.sal.fs.joinPaths(job.service.path, job.service.path)
         j.sal.fs.createDir(job.service.path)
         j.sal.fs.copyFile(path, args['key.path'])
         j.sal.fs.copyFile(path + '.pub', args['key.path'] + '.pub')
@@ -36,7 +36,7 @@ def input(job):
 
     if 'key.priv' not in args or args['key.priv'].strip() == "":
         print("lets generate private key")
-        args['key.path'] = j.sal.fs.joinPaths(job.service.path, "id_rsa")
+        args['key.path'] = j.sal.fs.joinPaths(job.service.path, job.service.name)
         j.sal.fs.createDir(job.service.path)
         j.sal.fs.remove(args['key.path'])
         cmd = "ssh-keygen -q -t rsa -f %s -N ''" % (args['key.path'])
